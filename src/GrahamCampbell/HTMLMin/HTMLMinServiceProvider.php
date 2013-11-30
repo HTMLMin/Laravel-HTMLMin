6b65ce261704ef62b8f1347948dc43e667757130
Graham Campbell <graham@mineuk.com>
2013-11-30 14:11:26 +0000

    Derp

--- src/GrahamCampbell/HTMLMin/HTMLMinServiceProvider.php
+++ src/GrahamCampbell/HTMLMin/HTMLMinServiceProvider.php
@@ -61,7 +61,7 @@ class HTMLMinServiceProvider extends ServiceProvider {
 
         $app->view->addExtension('blade.php', 'blade.php');
 
-        $app->after(function($request, $response) {
+        $app->after(function($request, $response) use ($app) {
             if($response instanceof \Illuminate\Http\Response) {
                 if ($response->headers->has('Content-Type') !== false) {
                     if (strpos($response->headers->get('Content-Type'), 'text/html') !== false) {
