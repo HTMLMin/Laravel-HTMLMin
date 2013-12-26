ccfac1ca99ba2137c380d69edc05477183480784
Graham Campbell <graham@mineuk.com>
2013-12-26 18:13:23 +0000

    Test refactor

--- tests/GrahamCampbell/Tests/HTMLMin/ServiceProviderTest.php
+++ tests/GrahamCampbell/Tests/HTMLMin/ServiceProviderTest.php
@@ -27,17 +27,7 @@ use GrahamCampbell\TestBench\Traits\ServiceProviderTestCaseTrait;
  * @license    https://github.com/GrahamCampbell/Laravel-HTMLMin/blob/develop/LICENSE.md
  * @link       https://github.com/GrahamCampbell/Laravel-HTMLMin
  */
-abstract class ServiceProviderTest extends AbstractTestCase
+class ServiceProviderTest extends AbstractTestCase
 {
     use ServiceProviderTestCaseTrait;
-
-    /**
-     * Get the service provider class.
-     *
-     * @return string
-     */
-    protected function getServiceProviderClass()
-    {
-        return 'GrahamCampbell\HTMLMin\HTMLMinServiceProvider';
-    }
 }
