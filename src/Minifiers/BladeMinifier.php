b197640dcf7f5a468c53ceab2c4ff272535ecbb6
William <gcphost@gmail.com>
2016-01-14 11:34:50 -0800

    Update BladeMinifier.php

--- src/Minifiers/BladeMinifier.php
+++ src/Minifiers/BladeMinifier.php
@@ -52,13 +52,30 @@ class BladeMinifier implements MinifierInterface
     {
         if ($this->shouldMinify($value)) {
             $replace = [
-                '/<!--[^\[](.*?)[^\]]-->/s' => '',
-                "/<\?php/"                  => '<?php ',
-                "/\n([\S])/"                => ' $1',
-                "/\r/"                      => '',
-                "/\n/"                      => '',
-                "/\t/"                      => ' ',
-                '/ +/'                      => ' ',
+                //remove tabs before and after HTML tags
+                '/\>[^\S ]+/s'   => '>',
+                '/[^\S ]+\</s'   => '<',
+                //shorten multiple whitespace sequences; keep new-line characters because they matter in JS!!!
+                '/([\t ])+/s'  => ' ',
+                //remove leading and trailing spaces
+                '/^([\t ])+/m' => '',
+                '/([\t ])+$/m' => '',
+                // remove JS line comments (simple only); do NOT remove lines containing URL (e.g. 'src="http://server.com/"')!!!
+                '~//[a-zA-Z0-9 ]+$~m' => '',
+                //remove empty lines (sequence of line-end and white-space characters)
+                '/[\r\n]+([\t ]?[\r\n]+)+/s'  => "\n",
+                //remove empty lines (between HTML tags); cannot remove just any line-end characters because in inline JS they can matter!
+                '/\>[\r\n\t ]+\</s'    => '><',
+                //remove "empty" lines containing only JS's block end character; join with next line (e.g. "}\n}\n</script>" --> "}}</script>"
+                '/}[\r\n\t ]+/s'  => '}',
+                '/}[\r\n\t ]+,[\r\n\t ]+/s'  => '},',
+                //remove new-line after JS's function or condition start; join with next line
+                '/\)[\r\n\t ]?{[\r\n\t ]+/s'  => '){',
+                '/,[\r\n\t ]?{[\r\n\t ]+/s'  => ',{',
+                //remove new-line after JS's line end (only most obvious and safe cases)
+                '/\),[\r\n\t ]+/s'  => '),',
+                //remove quotes from HTML attributes that does not contain spaces; keep quotes around URLs!
+                '~([\r\n\t ])?([a-zA-Z0-9]+)="([a-zA-Z0-9_/\\-]+)"([\r\n\t ])?~s' => '$1$2=$3$4', //$1 and $4 insert first white-space character found before/after attribute
             ];
 
             $value = preg_replace(array_keys($replace), array_values($replace), $value);
