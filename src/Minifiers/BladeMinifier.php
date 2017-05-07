1cad9d6059070ec30281a68410d53e388162e759
Bhavesh Ramburn <nitrogen@gmail.com>
2017-05-07 08:57:33 +0100

    bad boy minifier

--- src/Minifiers/BladeMinifier.php
+++ src/Minifiers/BladeMinifier.php
@@ -51,14 +51,35 @@ class BladeMinifier implements MinifierInterface
     public function render($value)
     {
         if ($this->shouldMinify($value)) {
+            //from http://stackoverflow.com/questions/5312349/minifying-final-html-output-using-regular-expressions-with-codeigniter
+            
+            $re = '%# Collapse whitespace everywhere but in blacklisted elements.
+        (?>             # Match all whitespans other than single space.
+          [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
+        | \s{2,}        # or two or more consecutive-any-whitespace.
+        ) # Note: The remaining regex consumes no text at all...
+        (?=             # Ensure we are not in a blacklist tag.
+          [^<]*+        # Either zero or more non-"<" {normal*}
+          (?:           # Begin {(special normal*)*} construct
+            <           # or a < starting a non-blacklist tag.
+            (?!/?(?:textarea|pre|script)\b)
+            [^<]*+      # more non-"<" {normal*}
+          )*+           # Finish "unrolling-the-loop"
+          (?:           # Begin alternation group.
+            <           # Either a blacklist start tag.
+            (?>textarea|pre|script)\b
+          | \z          # or end of file.
+          )             # End alternation group.
+        )  # If we made it here, we are not in a blacklist tag.
+        %Six';
+
             $replace = [
+
                 '/<!--[^\[](.*?)[^\]]-->/s' => '',
                 "/<\?php/"                  => '<?php ',
                 "/\n([\S])/"                => ' $1',
-                "/\r/"                      => '',
-                "/\n/"                      => '',
-                "/\t/"                      => ' ',
-                '/ +/'                      => ' ',
+                $re                         => ' ',
+
             ];
 
             $value = preg_replace(array_keys($replace), array_values($replace), $value);
@@ -95,10 +116,9 @@ class BladeMinifier implements MinifierInterface
      */
     protected function containsBadHtml($value)
     {
-        return preg_match('/<(code|pre|textarea)/', $value) ||
-            preg_match('/<script[^\??>]*>[^<\/script>]/', $value) ||
-            preg_match('/skip\.minification/', $value) ||
-            preg_match('/value=("|\')(.*)([ ]{2,})(.*)("|\')/', $value);
+        return 
+        preg_match('/skip\.minification/', $value) ||
+        preg_match('/value=("|\')(.*)([ ]{2,})(.*)("|\')/', $value);
     }
 
     /**
